8bffb3d8545cf15897127a5c4483f3fab8920b11
Aaron <aaron@doublemap.com>
2019-02-24 11:53:02 -0600

    added the funzies

--- js/app/views/RandomPokemonView.jsx
+++ js/app/views/RandomPokemonView.jsx
@@ -1,22 +1,48 @@
 import React from 'react'
 
+import { getPokeGif, capFirstLetter } from '../helpers/helper'
+
 const RandomPokemonView = (props) => {
 
     if (Object.entries(props.randomPokemon).length === 0 && props.randomPokemon.constructor === Object) {
         return null
     }
 
+    let pokeUrl = getPokeGif(props.randomPokemon.name)
+
+    if(pokeUrl === null) {
+        pokeUrl = (props.randomPokemon.sprites.front_default !== null ? props.randomPokemon.sprites.front_default : null)
+    }
+
+    if(props.randomPokemon.id === 258) {
+        pokeUrl = "https://i.kym-cdn.com/photos/images/original/000/056/244/Mudkip_baked_in_3D_by_cezkid.gif"
+        return (
+            <div id="random-pokemon-container">
+                <h1>I Herd U Liek Mudkipz</h1>
+                {   pokeUrl !== null ?
+                    <img src={ pokeUrl } className="alignSelfCenter pokeImg"/>
+                    :
+                    <p className="alignSelfCenter">no image found</p>
+                }
+                <h3 className="alignSelfCenter niceMarginSpacing">{ capFirstLetter(props.randomPokemon.name) }</h3>
+                <p className="alignSelfCenter niceMarginSpacing">Weight: { props.randomPokemon.weight }</p>
+                <p className="alignSelfCenter niceMarginSpacing">Height: { props.randomPokemon.height }</p>
+                <p className="alignSelfCenter niceMarginSpacing">Base XP: { props.randomPokemon.base_experience }</p>
+            </div>
+        )
+    }
+
     return (
         <div id="random-pokemon-container">
-            {   props.randomPokemon.sprites.front_default !== null ?
-                <img src={ props.randomPokemon.sprites.front_default } />
+            {   pokeUrl !== null ?
+                <img src={ pokeUrl } className="alignSelfCenter pokeImg"/>
                 :
-                <p >no image found</p>
+                <p className="alignSelfCenter">no image found</p>
             }
-            <h3>{ props.randomPokemon.name }</h3>
-            <p>Weight: { props.randomPokemon.weight }</p>
-            <p>Height: { props.randomPokemon.height }</p>
-            <p>Base XP: { props.randomPokemon.base_experience }</p>
+            <h3 className="alignSelfCenter niceMarginSpacing">{capFirstLetter( props.randomPokemon.name) }</h3>
+            <p className="alignSelfCenter niceMarginSpacing">Weight: { props.randomPokemon.weight }</p>
+            <p className="alignSelfCenter niceMarginSpacing">Height: { props.randomPokemon.height }</p>
+            <p className="alignSelfCenter niceMarginSpacing">Base XP: { props.randomPokemon.base_experience }</p>
         </div>
     )
 }
--- js/app/views/StarterPokemonView.jsx
+++ js/app/views/StarterPokemonView.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 
+import { getPokeGif, capFirstLetter } from '../helpers/helper'
+
 const StarterPokemonView = (props) => {
 
     const starters = props.starterPokemon.map((pokemon, index) => {
 
+        let pokeUrl = getPokeGif(pokemon.name)
+
+        if(pokeUrl === null) {
+            pokeUrl = (pokemon.sprites.front_default !== null ? pokemon.sprites.front_default : null)
+        }
+
         return (
-            <li id={pokemon.name} key={pokemon.name}>
-                {   pokemon.sprites.front_default !== null
-                        ? <img src={pokemon.sprites.front_default} />
-                        : <p>no image found</p>
+            <li id={pokemon.name} className="starterPokemons" key={pokemon.name}>
+                {   pokeUrl !== null
+                        ? <img src={ pokeUrl } className="alignSelfCenter pokeImg" />
+                        : <p className="alignSelfCenter">no image found</p>
                 }
-                <h3>{ pokemon.name }</h3>
-                <p>Height: { pokemon.height }</p>
-                <p>Weight: { pokemon.weight }</p>
-                <p>Base XP: { pokemon.base_experience }</p>
+                <h3 className="alignSelfCenter niceMarginSpacing">{ capFirstLetter(pokemon.name) }</h3>
+                <p className="alignSelfCenter niceMarginSpacing">Height: { pokemon.height }</p>
+                <p className="alignSelfCenter niceMarginSpacing">Weight: { pokemon.weight }</p>
+                <p className="alignSelfCenter niceMarginSpacing">Base XP: { pokemon.base_experience }</p>
             </li>
         )
     })
